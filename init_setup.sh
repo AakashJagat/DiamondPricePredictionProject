a069aad3f7394a5745b24cc404931656732f88e8
AakashJagat <aakash.jagat99@gmail.com>
2023-11-04 16:10:54 +0530

    new shell script

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating env with python 3.8 version"
-conda create --prefix ./env python=3.9 -y
+python -m venv env 
 echo [$(date)]: "activating the eneviornment"
-source activate ./env
+source env/Scripts/activate
 echo [$(date)]: "installing the dev requirements"
 pip install -r requirements.txt
 echo [$(date)]: "END"
