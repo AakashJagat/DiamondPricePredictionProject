fbbe8bcf86f322ccf9306104a7c446d2ade9b52b
AakashJagat <aakash.jagat99@gmail.com>
2023-11-21 02:02:41 +0530

    Wokrflow updated

--- app.py
+++ app.py
@@ -42,5 +42,8 @@ def predict_datapoint():
         return render_template("result.html",final_result=result)
     
 
+
+#execution begin
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8080)
+    
